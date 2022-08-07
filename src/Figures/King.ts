10e92d1c9d7fb553bec7749e50b73020730be1e0
Konstantin Atroschenko <kostay375298918971@gmail.com>
2022-08-07 19:45:00 +0300

    feat:Движение King короля

--- src/Figures/King.ts
+++ src/Figures/King.ts
@@ -14,6 +14,17 @@ export class King extends Figure {
   //логика движения фигуры
   canMove(target: Cell): boolean {
     if (!super.canMove(target)) return false; // Проверка на условия родительского canMove
-    return true;
+    //Расчет направления движения
+    const deltaX = Math.abs(this.cell.x - target.x);
+    const deltaY = Math.abs(this.cell.y - target.y);
+    // По диагонали фигура всегда смещается по x и y на 1-цу
+    // по вертикали смещение по x не происходит т.е. 0, а по y на единицу
+    // по горизонтали смещение по x на единицу, а по y не происходит т.е. 0
+    return (
+      (deltaX === 1 && deltaY === 1) ||
+      (deltaX === 1 && deltaY === 1) ||
+      (deltaX === 0 && deltaY === 1) ||
+      (deltaX === 1 && deltaY === 0)
+    );
   }
 }
